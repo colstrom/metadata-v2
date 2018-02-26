ea18abd1f94e0823592568979952ec3e195bec84
Chris Olstrom <chris@olstrom.com>
2018-02-26 15:37:43 -0800

    Adds Constant - SOCKET_PATHS
    This is a list of expected paths for the metadata socket.

---
+++ lib/metadata/v2/socket_paths.rb
@@ -0,0 +1,12 @@
+
+# -*- ruby -*-
+
+module Metadata
+  module V2
+    SOCKET_PATHS = [
+      "/.zonecontrol/metadata.sock",
+	    "/native/.zonecontrol/metadata.sock",
+	    "/var/run/smartdc/metadata.sock"
+    ].freeze
+  end
+end

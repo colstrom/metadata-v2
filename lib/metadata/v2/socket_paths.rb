e822d78005562a0bcd687447e6fe7159751ba67a
Chris Olstrom <chris@olstrom.com>
2018-03-20 19:11:38 -0700

    Whitespace cleanup

--- lib/metadata/v2/socket_paths.rb
+++ lib/metadata/v2/socket_paths.rb
@@ -5,8 +5,8 @@ module Metadata
   module V2
     SOCKET_PATHS = [
       "/.zonecontrol/metadata.sock",
-	    "/native/.zonecontrol/metadata.sock",
-	    "/var/run/smartdc/metadata.sock"
+      "/native/.zonecontrol/metadata.sock",
+      "/var/run/smartdc/metadata.sock"
     ].freeze
   end
 end
